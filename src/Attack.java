d4e574e72a7504237c76fde158263068dc502a90
jayathekdi <jayathekdi@gmail.com>
2022-09-09 08:32:57 -0500

    Completed Attack class

--- src/Attack.java
+++ src/Attack.java
@@ -1,2 +1,16 @@
 public class Attack {
+    private int damage;
+    private String name;
+    public Attack(String name, int damage) {
+        this.damage = damage;
+        this.name = name;
+    }
+    public void use(Pokemon opponent) {
+        if (damage >= opponent.getHealth()) {
+            opponent.setHealth(opponent.getHealth() - damage);
+        }
+        else {
+            opponent.setHealth(0);
+        }
+    }
 }
