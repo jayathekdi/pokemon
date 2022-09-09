347d5fd6369e0b9ba99ee8a46a2620698772c51e
skt29 <sarahkt29@gmail.com>
2022-09-09 15:46:13 -0500

    Completed Pokemon and some of Main class. Need to finish Main methods

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,36 @@
+import java.util.*;
 public class Main {
+    public static void main(String[] args) {
+        /*
+        Attack flameBall =
+        Attack
+        Attacks[] charmanderAttacks =
+        Attacks[] dittoAttacks =
+        Attacks[] evieAttacks =
+        Attacks[] squirtleAttacks =
+        Attacks[] pikachuAttacks =
+        Attacks[] snorlaxAttacks =
+        Pokemon charmander = new Pokemon("Charmander", 60, charmanderAttacks, false);
+        Pokemon ditto = new Pokemon("Ditto", 55, dittoAttacks, false);
+        Pokemon evie = new Pokemon("Evie", 3, evieAttacks, false);
+        Pokemon squirtle = new Pokemon("Squirtle", 28, squirtleAttacks, false);
+        Pokemon pikachu = new Pokemon("Pikachu", 110, pikachuAttacks, false);
+        Pokemon snorlax = new Pokemon("Snorlax", 50, snorlaxAttacks, false);
+         */
+        Scanner input = new Scanner(System.in);
+
+    }
+
+    public static void printBattle(Pokemon pokemon, Pokemon opponent){
+
+    }
+    public static void printPokemonMenu(Pokemon allPokemons){
+
+    }
+    public static void printBag(Item[] bag){
+
+    }
+    public static void printAttacksMenu(Attack[] attacks){
+
+    }
 }
--- src/Pokemon.java
+++ src/Pokemon.java
@@ -1,2 +1,52 @@
+import java.util.*;
 public class Pokemon {
+    private String name;
+    private int level;
+    private Attack[] attacks;
+    private int health;
+    private boolean isCurrent;
+    Pokemon(String name, int level, Attack[] attacks, boolean isCurrent){
+        this.name = name;
+        this.level = level;
+        this.attacks = attacks;
+        this.health = level * 3;
+        this.isCurrent = isCurrent;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public Attack[] getAttacks() {
+        return attacks;
+    }
+
+    public void setAttacks(Attack[] attacks) {
+        this.attacks = attacks;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public void attack(Attack attack, Pokemon opponent){
+        //opponent.setHealth() -= attack.getDamageLevel;
+        //
+    }
 }
