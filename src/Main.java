f5917fc6a79abf0cdfe358b3a9725ec944f06e9b
jayathekdi <jayathekdi@gmail.com>
2022-09-11 12:44:15 -0500

    Completed useItem and moveOpponent methods of Main class

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,16 @@
+import java.util.Random;
+
 public class Main {
+
+    public static void useItem(Item item, Pokemon pokemon) {
+        item.use(pokemon);
+    }
+
+    public static void moveOpponent(Pokemon opponent, Pokemon currentPokemon) {
+        Attack[] attacks = opponent.getAttacks();
+        Random random = new Random();
+        int num = random.nextInt(4);
+        attacks[num].use(currentPokemon);
+    }
+
 }
